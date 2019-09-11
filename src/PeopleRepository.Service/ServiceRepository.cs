e1febbb66beddb7412cda03a93111c2bf6f9528b
ojji <zshun86@gmail.com>
2019-09-11 12:07:03 +0200

    Add service repository to plugins

---
+++ src/PeopleRepository.Service/ServiceRepository.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Grpc.Net.Client;
+using People.Service;
+using PeopleRepository.Interface;
+using Person = PeopleRepository.Interface.Person;
+
+namespace PeopleRepository.Service
+{
+    public class ServiceRepository : IPeopleRepository
+    {
+        private readonly PeopleService.PeopleServiceClient _client;
+
+        public ServiceRepository(string host)
+        {
+            var channel = GrpcChannel.ForAddress(host);
+            _client = new PeopleService.PeopleServiceClient(channel);
+        }
+
+        public IEnumerable<Person> GetPeople()
+        {
+            var reply = _client.GetPeople(new GetPeopleRequest() {});
+            foreach (var person in reply.People)
+            {
+                yield return new Person
+                {
+                    FirstName = person.FirstName,
+                    LastName = person.LastName,
+                    Rating = person.Rating,
+                    StartDate = person.StartDate.ToDateTime()
+                };
+            }
+        }
+    }
+}
